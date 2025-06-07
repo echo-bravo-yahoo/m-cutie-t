fa0dcbe1942c822886fc93e4e554289ae6ab3912
Ashton Eby <ashton.eby@gmail.com>
2025-06-06 21:14:13 -0700

    wip

--- src/outputs/mqtt.js
+++ src/outputs/mqtt.js
@@ -34,7 +34,9 @@ export default class MQTT extends Output {
 
   async send(message) {
     const interpolatedTopic = this.interpolateConfigString(this.config.topic);
-    console.log(`Sending message to topic "${interpolatedTopic}":\n${JSON.stringify(message, null, 2)}
+    console.log(
+      `Sending message to topic "${interpolatedTopic}":\n${JSON.stringify(message, null, 2)}`
+    );
     this.mqtt.sendRaw(interpolatedTopic, JSON.stringify(message));
   }
 
